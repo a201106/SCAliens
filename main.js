ad97c4b0cb99f4b83d42ba887c10c8eb4a91265a
a201106 <77398406+a201106@users.noreply.github.com>
2021-11-28 19:41:40 +0530

    Update main.js

--- main.js
+++ main.js
@@ -23,30 +23,30 @@ function gotResults(error, results){
        document.getElementById("result_label").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
        document.getElementById("result_confidence").style.color = "rgb(" + random_number_r + "," + random_number_g + "," + random_number_b + ")";
 
-       img = document.getElementById("alien1");
+       img = document.getElementById("aliens1");
        img1 = document.getElementById("alien2");
        img2 = document.getElementById("alien3");
        img3 = document.getElementById("alien4");
 
        if (results[0].label == "Clap"){
-          img.src = "alien-01.gif";
+          img.src = "aliens-01.gif";
           img1.src = "aliens-02.png";
           img2.src = "aliens-03.png";
           img3.src = "aliens-04.png";
        } else if (results[0].label == "Humming"){
-        img.src = "alien-01.png";
+        img.src = "aliens-01.png";
         img1.src = "aliens-02.gif";
         img2.src = "aliens-03.png";
         img3.src = "aliens-04.png";
 
        } else if (results[0].label == "Whistling"){
-        img.src = "alien-01.png";
+        img.src = "aliens-01.png";
         img1.src = "aliens-02.png";
         img2.src = "aliens-03.gif";
         img3.src = "aliens-04.png";
        
     } else {
-        img.src = "alien-01.png";
+        img.src = "aliens-01.png";
         img1.src = "aliens-02.png";
         img2.src = "aliens-03.png";
         img3.src = "aliens-04.gif";
